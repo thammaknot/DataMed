9b2f866dcc461c7fb2d32674bb8e4bdf55dbaba1
Knot <thammaknot@gmail.com>
2017-06-26 23:07:58 +0700

    mid

---
+++ js/keys.js
@@ -0,0 +1,23 @@
+var patientKeys = {
+    'first_name': { 'display' : 'First name' },
+    'last_name': { 'display' : 'Last name' },
+    'phone' : { 'display' : 'Phone number' },
+    'dob' : { 'display' : 'Date of birth' },
+    'address' : { 'display' : 'Address' },
+    'email' : { 'display' : 'Email' },
+    'gender' : { 'display' : 'Gender' },
+};
+
+var visitKeys = {
+    'bp': { 'display': 'Blood Pressure' },
+    'hr': { 'display': 'Heart Rate' },
+    'weight': { 'display': 'Weight' },
+    'height': { 'display': 'Height' },
+    'symptoms': { 'display': 'Symptoms' },
+    'diagnosis': { 'display': 'Diagnosis' },
+    'prescriptions': { 'display': 'Prescriptions' },
+    'therapy': { 'display': 'Therapy' },
+    'cost': { 'display': 'Cost'},
+    'next_visit': { 'display': 'Next Visit' },
+    'remarks' : { 'display': 'Remarks' },
+};
---
+++ js/visits.js
@@ -0,0 +1,9 @@
+
+
+var displayFullVisit = function(info) {
+    var mainPanel = $('#main');
+
+    for (var key in patientKeys) {
+
+    }
+};
